4b8f89ac39a935afdfdfa27aee718a2ab203239b
Afzal Ali <afzalali1999@gmail.com>
2020-12-22 18:05:21 -0500

    upload to repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </BrowserRouter>
+  );
+}
+
+export default App;
